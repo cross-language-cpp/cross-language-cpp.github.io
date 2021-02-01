12e60f2d3899a86a7918fc4e21af6eb4a045d4dd
jothepro <github@jothe.pro>
2021-02-01 22:27:21 +0100

    Fix versioning (#14)
    * fix version tags

--- configure.sh
+++ configure.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git submodule foreach 'echo "export VERSION_${name//-/_}=$(git describe --tags)" >> ../.env'
+git submodule foreach 'echo "export VERSION_$(echo $name | tr - _)=$(git describe --tags)" >> ../.env'
 source .env
 envsubst < mkdocs.yml.in > mkdocs.yml
